a93e7f367890ae546d9e5bc809b02402b1bd976d
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2021-11-14 16:39:54 +0200

    made the exists katigorimata

--- Άσκηση 1/askisi1.pl
+++ Άσκηση 1/askisi1.pl
@@ -1,1 +1,3 @@
 family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)), person(ann, fox, date(9, may, 1951), unemployed), [person(pat, fox, date(5, may, 1973), unemployed), person(jim, fox, date(5, may, 1973), unemployed)]).
+exists(X) :- family(X, Y, Z).
+exists(Y) :- family(X, Y, Z).
