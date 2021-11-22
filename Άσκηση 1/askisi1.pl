5719854ddb7bf0f684daad48d0191aac6eb5c8a4
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-11-22 20:10:16 +0200

    ΦΤΑΝΟΥΜΕ ΣΕ ΕΝΑ ΤΕΛΟΣ

--- Άσκηση 1/askisi1.pl
+++ Άσκηση 1/askisi1.pl
@@ -1,14 +1,22 @@
-family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                person(ann, fox, date(9, may, 1951), unemployed),      [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
-family(person(manos, seimenis, date(31, march, 1962), works(archirodon, 2000)), person(vicky, rori, date(13, june, 1965), unemployed), [person(aimilios, seimenis, date(13, october, 1997), unemployed), person(giorgos, seimenis, date(26, october, 2001), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+family(person(manos, seimenis,  date(31, march, 1962), works(archirodon, 2000)),     person(vicky, rori, date(13, june, 1965), unemployed),                      [person(aimilios, seimenis, date(13, october, 1997), unemployed), person(giorgos, seimenis, date(26, october, 2001), unemployed)]).
+family(person(evangelos, manolatos, date(31, march, 1942), works(archirodon, 5000)), person(theodora, manolatou, date(13, june, 1939), works(cleaner, 3000)),    []).
 
 /* Ορίζουμε το κατηγόρημα exists να ορίζει κάθε γονέα. */
-exists(X) :- family(X, Y, Z).
-exists(Y) :- family(X, Y, Z).
+exists(X)         :- family(X, Y, Z).
+exists(Y)         :- family(X, Y, Z).
 
 /* Ορίζουμε το κατηγόρημα dateofbirth να υπάρχει για κάθε γονέα. */
-dateofbirth(X, Y) :- family(person(A, B, date(C, X, Y), D), E, F).
-dateofbirth(X, Y) :- family(E, person(A, B, date(C, X, Y), D), F).
+%dateofbirth(X, Y) :- family(person(A, B, date(C, X, Y), D), E, F).
+%dateofbirth(X, Y) :- family(E, person(A, B, date(C, X, Y), D), F).
+
+dateofbirth(X, Y) :- exists(person(A, B, date(C, X, Y), D)).
 
 /* Ορίζουμε το κατηγόρημα salary να υπάρχει για κάθε εργαζόμενο γονέα. */
-salary(X, Y) :- family(person(A, B, C, works(X, Y)), W, F).
-salary(X, Y) :- family(H, person(A, B, C, works(X, Y)), F).
+%salary(X, Y) :- family(person(A, B, C, works(X, Y)), W, F).
+%salary(X, Y) :- family(H, person(A, B, C, works(X, Y)), F).
+
+salary(X, Y) :- exists(person(A, B, C, works(X, Y))).
+
+/* ΑΥΤΟ ΔΟΥΛΕΥΕΙ!!! */
+%exists(person(FIRSTNAME, LASTNAME, date(DAY, MONTH, YEAR), works(PLACE, SALARY))), dateofbirth(MONTH, YEAR), salary(PLACE, SALARY), X < 1950, Y < 8000.
