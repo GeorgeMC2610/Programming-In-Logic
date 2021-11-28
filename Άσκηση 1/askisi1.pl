cf643ab36fe2e0f6fb99716b21c9108045e15db4
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-11-28 15:55:00 +0200

    προστίθονται οικογένειες σιγά-σιγά

--- Άσκηση 1/askisi1.pl
+++ Άσκηση 1/askisi1.pl
@@ -1,6 +1,11 @@
 family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
-family(person(manos, seimenis,  date(31, march, 1962), works(archirodon, 2000)),     person(vicky, rori, date(13, june, 1965), unemployed),                      [person(aimilios, seimenis, date(13, october, 1997), unemployed), person(giorgos, seimenis, date(26, october, 2001), unemployed)]).
-family(person(evangelos, manolatos, date(31, march, 1942), works(archirodon, 5000)), person(theodora, manolatou, date(13, june, 1939), works(cleaner, 3000)),    []).
+family(person(john, wick, date(19, october, 1978), works(meta, 65000)),              person(julia, wick, date(31, january, 1982), works(razer, 10150)),          [person(marianne, wick, date(1, february, 2009), unemployed),     person(patrick, wick, date(29, july, 2011), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+family(person(torn, fox, date(7, may, 1950), works(bbe, 15200)),                     person(ann, fox, date(9, may, 1951), unemployed),                           [person(pat, fox, date(5, may, 1973), unemployed),                person(jim, fox, date(5, may, 1973), unemployed)]).
+
 
 /* Ορίζουμε το κατηγόρημα exists να ορίζει κάθε γονέα. */
 exists(X)         :- family(X, Y, Z).
@@ -19,4 +24,4 @@ dateofbirth(X, Y) :- exists(person(A, B, date(C, X, Y), D)).
 salary(X, Y) :- exists(person(A, B, C, works(X, Y))).
 
 /* ΑΥΤΟ ΔΟΥΛΕΥΕΙ!!! */
-%exists(person(FIRSTNAME, LASTNAME, date(DAY, MONTH, YEAR), works(PLACE, SALARY))), dateofbirth(MONTH, YEAR), salary(PLACE, SALARY), X < 1950, Y < 8000.
+%exists(person(FIRSTNAME, LASTNAME, date(DAY, MONTH, YEAR), works(PLACE, SALARY))), dateofbirth(MONTH, YEAR), salary(PLACE, SALARY), YEAR < 1950, SALARY < 8000.
