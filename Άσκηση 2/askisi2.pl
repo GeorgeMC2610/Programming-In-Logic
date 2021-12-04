519a77c6eeccc27259c41ad95651919ab7e17e67
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-12-04 19:19:52 +0200

    ΝΑΝΙ; ΔΟΥΛΕΥΕΙ ΠΕΡΙΟΔ ΚΟΥΣΤΕΙΟΝΜΑΡΚ;

--- Άσκηση 2/askisi2.pl
+++ Άσκηση 2/askisi2.pl
@@ -1,3 +1,6 @@
 /* Ορίζουμε, πρώτα, δύο κατηγορήματα που θα ισχύουν και την κενή λίστα και για την λίστα που έχει μονάχα ένα στοιχείο. */
 common_list([], []).
 common_list([X], [X]).
+
+common_list([X | L1], [Y | L2]):- X =:= Y; common_list(L1, L2).
+common_list([X, Y | L1], [Z, A | L2]):- X =:= Z; X =:= A; Y =:= Z; Y =:= A; common_list(L1, L2).
