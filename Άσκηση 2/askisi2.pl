cf28c64a11553ac4fade8414234002a371fd6c9b
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-12-04 19:30:26 +0200

    I THOUGHT HAHA

--- Άσκηση 2/askisi2.pl
+++ Άσκηση 2/askisi2.pl
@@ -2,6 +2,5 @@
 common_list([], []).
 common_list([X], [X]).
 
-/* Αρκεί να ορίσουμε δύο φορές τι ισχύει για δύο λίστες που έχουν ίδιο στοιχείο */
-common_list([X | L1], [Y | L2]):- X =:= Y; common_list(L1, L2).
-common_list([X, Y | L1], [Z, A | L2]):- X =:= Z; X =:= A; Y =:= Z; Y =:= A; common_list(L1, L2).
+common_list([H | L1], [K | L2]):- member(K, L1), common_list(L1, L2).
+common_list([H | L1], [K | L2]):- member(H, L2), common_list(L1, L2).
