f28bb3898b610ce60e6590ae0019df95acede419
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-12-22 19:02:09 +0200

    IT'S COMING TO AN END

--- Άσκηση 2/askisi2.pl
+++ Άσκηση 2/askisi2.pl
@@ -1,5 +1,6 @@
 /* Ορίζουμε, πρώτα, δύο κατηγορήματα που θα ισχύουν και την κενή λίστα και για την λίστα που έχει μονάχα ένα στοιχείο. */
-common_list([], []).
-common_list([X], [X]).
+element_in_list(X, [X]).
+element_in_list(X, [Y | T]) :- X =:= Y; element_in_list(X, T).
 
-common_list(L1, L2) :- member(E, L1), member(E, L2).
+common_list([X], [X]).
+common_list([X | T1], [Y | T2]) :- element_in_list(X, [Y | T2]); element_in_list(Y, [X | T1]); common_list(T1, T2).
