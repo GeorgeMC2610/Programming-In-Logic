b0367cbe8361c2598cd78c3efa88f5dc79b203b2
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-12-22 21:57:11 +0200

    έβαλα σχόλια

--- Άσκηση 2/askisi2.pl
+++ Άσκηση 2/askisi2.pl
@@ -1,6 +1,7 @@
-/* Ορίζουμε, πρώτα, δύο κατηγορήματα που θα ισχύουν και την κενή λίστα και για την λίστα που έχει μονάχα ένα στοιχείο. */
+/* First, we make a function which returns true if an element exists in a list. */
 element_in_list(X, [X]).
 element_in_list(X, [Y | T]) :- X =:= Y; element_in_list(X, T).
 
+/* Then, we use recursion to find if two lists have at least one common element. The function above helps us a lot, as we recursively look every sublist. */
 common_list([X], [X]).
 common_list([X | T1], [Y | T2]) :- element_in_list(X, [Y | T2]); element_in_list(Y, [X | T1]); common_list(T1, T2).
