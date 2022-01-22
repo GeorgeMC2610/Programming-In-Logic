ab203535334c442762254b8256522c1b081d368e
GeorgeMC2610 <giorgosseimenis@gmail.com>
2022-01-22 13:42:26 +0200

    κάνω το σωστό; (questionmark)

---
+++ Άσκηση 3/askisi3.pl
@@ -0,0 +1,17 @@
+/* πρώτα συνδέουμε τους κόμβους μεταξύ τους */
+
+connect(start, 2).
+
+connect(2, 3).
+connect(2, 8).
+
+connect(3, 9).
+connect(3, 4).
+
+connect(4, 10).
+
+connect(10, 16).
+
+connect(16, 22).
+
+connect(22, 21).
