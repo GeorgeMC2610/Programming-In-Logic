06c945d6d63044852a4bab9c89385b1a3c6b3cff
GeorgeMC2610 <giorgosseimenis@gmail.com>
2022-01-22 14:07:03 +0200

    είμαστε στο 32
    θα 'χε πλάκα να κοπεί το ρεύμα όσο δουλεύω

--- Άσκηση 3/askisi3.pl
+++ Άσκηση 3/askisi3.pl
@@ -53,13 +53,74 @@ connect(12, 18).
 connect(13, 7).
 connect(13, 14).
 
+%14
+connect(14, 13).
+connect(14, 15).
+connect(14, 20).
+
+%15
+connect(15, 14).
+connect(15, 21).
+
+%16
+connect(16, 10).
 connect(16, 22).
 
+%17
+connect(17, 11).
+connect(17, 23).
+
+%18
+connect(18, 12).
+connect(18, 24).
+
+%19
+connect(19, 25).
+
+%20
+connect(20, 14).
+connect(20, 26).
+
+%21
+connect(21, 15).
+connect(21, 22).
+
+%22
+connect(22, 16).
 connect(22, 21).
 
-connect(21, 15).
+%23
+connect(23, 17).
+connect(23, 29).
 
-connect(15, 14).
+%24
+connect(24, 18).
+connect(24, 30).
 
-connect(14, 20).
-connect(14, 13).
+%25
+connect(25, 19).
+connect(25, 31).
+
+%26
+connect(26, 20).
+connect(26, 27).
+
+%27
+connect(27, 26).
+connect(27, 28).
+
+%28
+connect(28, 27).
+connect(28, 29).
+
+%29
+connect(29, 23).
+connect(29, 28).
+
+%30
+connect(30, 24).
+connect(30, 36).
+
+%31
+connect(31, 25).
+connect(31, 32).
